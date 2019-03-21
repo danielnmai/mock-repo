22c16bdbf715163e31f3061c800d1570acf8b1d8
Daniel Mai <danielmai1728@gmail.com>
2019-03-21 20:29:17 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -34,6 +34,6 @@ public class Cipmyt {
         System.out.println("kdcck");
         System.out.println("pchvx");
         System.out.println("muxuf");
-        System.out.println("wlkns");
+        System.out.println("fucnw");
     }
 }
