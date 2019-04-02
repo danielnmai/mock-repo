b429883d4373bf7a791c6119085cf500effa280a
Daniel Mai <danielmai1728@gmail.com>
2019-04-02 17:27:27 +0000

    add code in files types: .xml,.java,.java},.js,.js} remove code in files types: .xml,.java,.java},.js,.js}

--- content.java
+++ content.java
@@ -25,6 +25,6 @@ public class Cipmyt {
         System.out.println("whgci");
         System.out.println("jopal");
         System.out.println("pmosx");
-        System.out.println("gmnco");
+        System.out.println("ytiyi");
     }
 }
