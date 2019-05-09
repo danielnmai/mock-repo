7321216d7da594f1d1d08b89bb1a3be8f8555b95
Daniel Mai <danielmai1728@gmail.com>
2019-05-09 20:35:12 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -3,6 +3,6 @@ public class Cktcdw {
         System.out.println("xepij");
         System.out.println("gpvgt");
         System.out.println("hfewm");
-        System.out.println("rpkjt");
+        System.out.println("ltccr");
     }
 }
