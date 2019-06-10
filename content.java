5df962917ece372dcbdd36a8f264f908d323adee
Daniel Mai <danielmai1728@gmail.com>
2019-06-10 18:09:32 +0000

    add code in files types: .js,.md,.gradle,.java,.json,.lock remove code in files types: .js,.gradle,.java,.json,.lock

--- content.java
+++ content.java
@@ -32,6 +32,6 @@ public class Cipmyt {
         System.out.println("vifur");
         System.out.println("fsnyl");
         System.out.println("kdcck");
-        System.out.println("xsjsg");
+        System.out.println("yaxoy");
     }
 }
