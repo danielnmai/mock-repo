a5964bf3646df596a9975d2c9a6f7aa07205310a
Daniel Mai <danielmai1728@gmail.com>
2019-06-11 18:16:38 +0000

    add code in files types: .java,.json remove code in files types: .java

--- content.java
+++ content.java
@@ -46,6 +46,6 @@ public class Cipmyt {
         System.out.println("ururm");
         System.out.println("jgcfl");
         System.out.println("wuhvi");
-        System.out.println("rvhex");
+        System.out.println("gpehw");
     }
 }
