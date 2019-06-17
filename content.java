e8628b1a69fbcdb03ac8adf7d9792d6eccdad1f3
Daniel Mai <danielmai1728@gmail.com>
2019-06-17 17:09:09 +0000

    add code in files types: .gradle,.java remove code in files types: .java

--- content.java
+++ content.java
@@ -64,6 +64,6 @@ public class Cipmyt {
         System.out.println("lqygf");
         System.out.println("fpkpe");
         System.out.println("pmvrg");
-        System.out.println("wllve");
+        System.out.println("tlqpe");
     }
 }
