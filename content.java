aba4ce168cec47f2ffd3271bec27bedf67785378
Daniel Mai <danielmai1728@gmail.com>
2019-06-26 21:29:51 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cobskc {
     public static void main() {
         System.out.println("niqcl");
         System.out.println("dbogp");
-        System.out.println("qbxdw");
+        System.out.println("pnsxq");
     }
 }
