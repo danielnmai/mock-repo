b8558bb18ed00d563e143c51f5bdc43826219a81
Daniel Mai <danielmai1728@gmail.com>
2019-06-27 19:02:07 +0000

    add code in files types: .gradle,.java,.properties remove code in files types:

--- content.java
+++ content.java
@@ -46,5 +46,6 @@ public class Cipmyt {
         System.out.println("tltfm");
         System.out.println("jrbaa");
         System.out.println("wfeab");
+        System.out.println("vhpmg");
     }
 }
