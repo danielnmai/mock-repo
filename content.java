1a4b55230e04333d8e0a341a151e72f42ef01059
Daniel Mai <danielmai1728@gmail.com>
2019-06-27 19:02:07 +0000

    add code in files types: .gradle,.java,.properties remove code in files types:

--- content.java
+++ content.java
@@ -3,5 +3,6 @@ public class Cobskc {
         System.out.println("niqcl");
         System.out.println("dbogp");
         System.out.println("ixrhp");
+        System.out.println("pijyj");
     }
 }
