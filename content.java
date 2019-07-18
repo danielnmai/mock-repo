26d28e49cedf60232c9d9a3f8b0f95cb91d27900
Daniel Mai <danielmai1728@gmail.com>
2019-07-18 23:01:34 +0000

    add code in files types: .gradle,.java,.xml remove code in files types: .java,.xml

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cobskc {
     public static void main() {
         System.out.println("niqcl");
         System.out.println("dbogp");
-        System.out.println("dfgqo");
+        System.out.println("vugwr");
     }
 }
