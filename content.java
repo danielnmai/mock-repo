463d81663a61d3407d57e634d30998da372031ba
Daniel Mai <danielmai1728@gmail.com>
2019-07-17 21:49:22 -0700

    add code in files types: .java removed code in files types: .java

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cyqlno {
     public static void main() {
         System.out.println("meqqw");
         System.out.println("mgjrr");
-        System.out.println("cjfui");
+        System.out.println("rvxrq");
     }
 }
