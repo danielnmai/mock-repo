9164a95da624dcafec3d944366f0de540d218256
Daniel Mai <danielmai1728@gmail.com>
2019-07-22 17:19:40 -0700

    add code in files types: .gradle,.java removed code in files types: .gradle,.java

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cyqlno {
     public static void main() {
         System.out.println("meqqw");
         System.out.println("mgjrr");
-        System.out.println("eendo");
+        System.out.println("qwmnc");
     }
 }
