bb0990996721584aa342363526da25c46c854606
Daniel Mai <danielmai1728@gmail.com>
2019-07-24 17:58:18 +0000

    add code in files types: .java,.xml remove code in files types: .java,.xml

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cobskc {
     public static void main() {
         System.out.println("niqcl");
         System.out.println("dbogp");
-        System.out.println("paqoc");
+        System.out.println("iprnp");
     }
 }
