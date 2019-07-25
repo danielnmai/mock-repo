35b848d757b30d95051a20bc5fe8b7e9c4656308
Daniel Mai <danielmai1728@gmail.com>
2019-07-24 17:58:18 -0700

    add code in files types: .java,.xml removed code in files types: .java,.xml

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cyqlno {
     public static void main() {
         System.out.println("meqqw");
         System.out.println("mgjrr");
-        System.out.println("nqmnb");
+        System.out.println("rsodn");
     }
 }
