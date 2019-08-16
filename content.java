6b4a064e8ae28a355b950178a852fd8f70487cef
Daniel Mai <danielmai1728@gmail.com>
2019-08-15 17:22:46 -0700

    add code in files types: .java,.xml removed code in files types: .java,.xml

--- content.java
+++ content.java
@@ -4,6 +4,6 @@ public class Cktcdw {
         System.out.println("gpvgt");
         System.out.println("hfewm");
         System.out.println("emlrs");
-        System.out.println("fpwec");
+        System.out.println("ppmfd");
     }
 }
