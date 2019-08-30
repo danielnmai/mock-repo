99169b8434d3f4c58620bc09aa0bed2e80127e07
Daniel Mai <danielmai1728@gmail.com>
2019-08-29 17:44:44 -0700

    add code in files types: .gradle,.xml,.java removed code in files types: .xml,.java

--- content.java
+++ content.java
@@ -6,6 +6,6 @@ public class Cktcdw {
         System.out.println("emlrs");
         System.out.println("ihrsu");
         System.out.println("bbxoy");
-        System.out.println("yquql");
+        System.out.println("nexxh");
     }
 }
