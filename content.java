b745f44436e44b64903f557d62e9234aa4ff3060
Daniel Mai <danielmai1728@gmail.com>
2019-09-03 21:08:32 +0000

    add code in files types: .gradle,.java,.properties,.json remove code in files types: .gradle,.java,.json

--- content.java
+++ content.java
@@ -49,6 +49,6 @@ public class Cipmyt {
         System.out.println("gspin");
         System.out.println("ydsdd");
         System.out.println("bhigy");
-        System.out.println("ojikt");
+        System.out.println("jlaog");
     }
 }
