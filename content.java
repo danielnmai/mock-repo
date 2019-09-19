278e6af56f01b509e782f135b63a41e2537d8c77
Daniel Mai <danielmai1728@gmail.com>
2019-09-19 20:05:23 +0000

    add code in files types: .java,.js remove code in files types: .bundle,.java,.js

--- content.java
+++ content.java
@@ -70,6 +70,6 @@ public class Cipmyt {
         System.out.println("wkpfv");
         System.out.println("htyml");
         System.out.println("ihrty");
-        System.out.println("nxsqc");
+        System.out.println("whemq");
     }
 }
