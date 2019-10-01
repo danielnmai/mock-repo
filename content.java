3d2405fcd66e80a1a9b5bac9ef738d1d7401fd05
Daniel Mai <danielmai1728@gmail.com>
2019-10-01 23:28:00 +0000

    add code in files types: ,.xml,.bundle,.java,.js remove code in files types: ,.xml,.bundle,.java

--- content.java
+++ content.java
@@ -49,6 +49,6 @@ public class Cipmyt {
         System.out.println("gspin");
         System.out.println("ydsdd");
         System.out.println("bhigy");
-        System.out.println("ugxqc");
+        System.out.println("pavmi");
     }
 }
