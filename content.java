1edd7e2c1c53cacf1dec14652af24096fac3be2c
Daniel Mai <danielmai1728@gmail.com>
2019-11-04 22:10:13 -0800

    add code in files types: .md,.js,.gradle,.java,.java},.json,.sh,.lock removed code in files types: .md,.js,.gradle,.java,.java},.json,.lock

--- content.java
+++ content.java
@@ -4,6 +4,6 @@ public class Cktcdw {
         System.out.println("gpvgt");
         System.out.println("hfewm");
         System.out.println("emlrs");
-        System.out.println("hvetg");
+        System.out.println("mrsco");
     }
 }
