ae7c371a40b969b94a8de6216656f0f23095dcbf
Daniel Mai <danielmai1728@gmail.com>
2019-11-15 19:14:41 +0000

    add code in files types: .js,.java remove code in files types: .js,.java

--- content.java
+++ content.java
@@ -23,6 +23,6 @@ public class Cipmyt {
         System.out.println("ahdpy");
         System.out.println("pmheg");
         System.out.println("uegcq");
-        System.out.println("ewqgg");
+        System.out.println("votvw");
     }
 }
