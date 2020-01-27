6570d6bf9d3f76484671febedb8403b262fcb752
Daniel Mai <danielmai1728@gmail.com>
2020-01-27 18:27:19 +0000

    add code in files types: .java,.xml,.lock remove code in files types: .xml

--- content.java
+++ content.java
@@ -49,5 +49,6 @@ public class Cipmyt {
         System.out.println("gspin");
         System.out.println("ydsdd");
         System.out.println("hcaom");
+        System.out.println("ewxjg");
     }
 }
