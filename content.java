0828c5235da16c748d7380d8920eba35c3600ea6
Daniel Mai <danielmai1728@gmail.com>
2020-09-02 00:34:23 +0000

    add code in files types: .gradle,.java,.json,.js,.lock remove code in files types: .gradle,.js,.lock

--- content.java
+++ content.java
@@ -33,5 +33,6 @@ public class Cipmyt {
         System.out.println("fsnyl");
         System.out.println("kdcck");
         System.out.println("gysih");
+        System.out.println("derdj");
     }
 }
