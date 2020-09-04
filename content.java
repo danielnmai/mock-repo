ba0dedeeb5405b20f47ff240b594c76a1d8e086c
Daniel Mai <danielmai1728@gmail.com>
2020-09-04 23:03:06 +0000

    add code in files types: .bundle,.java,.xml,.gradle,.json remove code in files types: .bundle,.java,.xml,.gradle,.json

--- content.java
+++ content.java
@@ -79,6 +79,6 @@ public class Cipmyt {
         System.out.println("qnpah");
         System.out.println("trgjc");
         System.out.println("asvyc");
-        System.out.println("vugml");
+        System.out.println("rigme");
     }
 }
